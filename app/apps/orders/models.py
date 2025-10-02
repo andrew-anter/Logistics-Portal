26b14d3a76708c422a210aea85b68886c7084dc0
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 02:03:07 +0300

    created Product model

--- app/apps/orders/models.py
+++ app/apps/orders/models.py
@@ -1,3 +1,23 @@
+import uuid
+
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    class Status(models.IntegerChoices):
+        PENDING = 1
+        PROCESSING = 2
+        APPROVED = 3
+        FAILED = 4
+
+    reference_code = models.UUIDField(default=uuid.uuid4)
+    product = models.ForeignKey(to="products.Product", on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=0)
+    status = models.IntegerField(choices=Status)  # pyright: ignore
+    created_by = models.ForeignKey("users.Profile", on_delete=models.CASCADE)
+    has_been_processed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"Order {self.reference_code}"
