5ec228e4c8f8f25f60fc126c95597021ebc9c2cc
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 01:49:15 +0300

    created product model

--- app/apps/products/models.py
+++ app/apps/products/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    sku = models.UUIDField()
+    stock_quantity = models.IntegerField(default=0)
+    company = models.ForeignKey(to="companies.Company", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self) -> str:
+        return f"{self.sku}"
