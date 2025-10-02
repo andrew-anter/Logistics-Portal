29e24ab14e536597a1440fe124cf24875950faba
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 01:49:21 +0300

    created user model

--- app/apps/users/models.py
+++ app/apps/users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import Group, User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    company = models.ForeignKey(
+        to="companies.Company",
+        on_delete=models.CASCADE,
+    )
+    role = models.ForeignKey(to=Group, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self) -> str:
+        return self.user.get_full_name()
