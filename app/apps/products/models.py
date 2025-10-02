c7a1313bb993ac0fd1519cba4f9d06a3262cfbba
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 01:53:16 +0300

    updated the uuid field to use uuid4 as its default value

--- app/apps/products/models.py
+++ app/apps/products/models.py
@@ -1,12 +1,14 @@
+import uuid
+
 from django.db import models
 
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
-    sku = models.UUIDField()
+    sku = models.UUIDField(default=uuid.uuid4)
     stock_quantity = models.IntegerField(default=0)
     company = models.ForeignKey(to="companies.Company", on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
 
     def __str__(self) -> str:
-        return f"{self.sku}"
+        return self.name
