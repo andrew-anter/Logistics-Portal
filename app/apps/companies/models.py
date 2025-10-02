bc0d0501251d6ad70d29651a49ea2edd3932d533
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 01:49:10 +0300

    created company model

--- app/apps/companies/models.py
+++ app/apps/companies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+    domain = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True, verbose_name="Active")
+
+    def __str__(self) -> str:
+        return self.name
