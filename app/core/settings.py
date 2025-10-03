caa7e96410e17daee94041ab8e6769923aaf08c4
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 22:22:01 +0300

    rearranged the middlewares

--- app/core/settings.py
+++ app/core/settings.py
@@ -48,12 +48,12 @@ if DEBUG:
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "apps.companies.middleware.TenantMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "apps.companies.middleware.TenantMiddleware",
 ]
 
 ROOT_URLCONF = "core.urls"
