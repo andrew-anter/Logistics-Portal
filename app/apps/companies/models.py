a19899fc59d16627b5d627d26c0b952bf9d64ed3
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 12:02:26 +0300

    enforced domain name to be unique per company

--- app/apps/companies/models.py
+++ app/apps/companies/models.py
@@ -5,7 +5,12 @@ from .validators import validate_subdomain_name
 
 class Company(models.Model):
     name = models.CharField(max_length=50)
-    domain = models.CharField(max_length=50, validators=[validate_subdomain_name])
+    domain = models.CharField(
+        max_length=50,
+        unique=True,
+        db_index=True,
+        validators=[validate_subdomain_name],
+    )
     is_active = models.BooleanField(default=True, verbose_name="Active")
 
     def __str__(self) -> str:
