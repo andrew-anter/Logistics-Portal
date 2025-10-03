b9ff7a3211a58283b182b8f954bcbeb9d4509a6b
Andrew Anter <andrew.anter@gmail.com>
2025-10-03 17:21:07 +0300

    changed has_been_processed to be a property rather than a field

--- app/apps/orders/models.py
+++ app/apps/orders/models.py
@@ -17,7 +17,6 @@ class Order(models.Model):
     quantity = models.IntegerField(default=0)
     status = models.IntegerField(choices=Status)  # pyright: ignore
     created_by = models.ForeignKey("users.Profile", on_delete=models.CASCADE)
-    has_been_processed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     company = models.ForeignKey(to="companies.Company", on_delete=models.CASCADE)
@@ -25,3 +24,9 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return f"Order {self.reference_code}"
+
+    @property
+    def has_been_processed(self) -> bool:
+        return (
+            self.status == Order.Status.APPROVED or self.status == Order.Status.FAILED
+        )
