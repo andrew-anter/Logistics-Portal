6fedf5c19735844eab877b7c84437638efc1744b
Andrew Anter <andrew.anter@gmail.com>
2025-10-04 10:58:35 +0300

    added password validation to repsect the global password validators

--- app/apps/users/forms.py
+++ app/apps/users/forms.py
@@ -1,5 +1,6 @@
 from core.thread_locals import get_current_tenant
 from django import forms
+from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError as CoreValidationError
 
 from .models import Profile
@@ -31,38 +32,45 @@ class ProfileAdminForm(forms.ModelForm):
         else:
             self.fields["password"].required = True
 
+    def clean_password(self):  # noqa: ANN201
+        password = self.cleaned_data.get("password")
+
+        if not self.instance.pk or password:
+            try:
+                password_validation.validate_password(password=password)  # pyright: ignore[reportArgumentType]
+            except CoreValidationError as e:
+                raise forms.ValidationError(e.messages)  # pyright: ignore[reportArgumentType]
+
+        return password
+
     def save(self, commit=True):  # noqa: ANN001, ANN201, FBT002
         if "company" not in self.cleaned_data:
             company = get_current_tenant()
         else:
             company = self.cleaned_data["company"]
-
-        super().save(commit=False)
         if not self.instance.pk:
-            try:
-                profile = create_profile_service(
-                    username=self.cleaned_data["username"],
-                    email=self.cleaned_data["email"],
-                    first_name=self.cleaned_data["first_name"],
-                    last_name=self.cleaned_data["last_name"],
-                    company=company,
-                    role=self.cleaned_data["role"],
-                    password=self.cleaned_data["password"],
-                )
-                self.instance = profile
-            except CoreValidationError as e:
-                self.add_error("password", e)
+            profile = create_profile_service(
+                username=self.cleaned_data["username"],
+                email=self.cleaned_data["email"],
+                first_name=self.cleaned_data["first_name"],
+                last_name=self.cleaned_data["last_name"],
+                company=company,
+                role=self.cleaned_data["role"],
+                password=self.cleaned_data["password"],
+            )
+            self.instance = profile
 
         else:
             profile = super().save(commit=False)
             user = profile.user
-
             user.username = self.cleaned_data["username"]
             user.email = self.cleaned_data["email"]
             user.first_name = self.cleaned_data["first_name"]
             user.last_name = self.cleaned_data["last_name"]
+            user.set_password(self.cleaned_data["password"])
 
             if commit:
+                super().save(commit=False)
                 user.save()
                 profile.save()
                 self.save_m2m()
